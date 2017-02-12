ff6105cb17a825b3a0c601daba9ee18ab79d13d1
NoGameNoLife <NoGameNo_Life@163.com>
2017-02-12 14:05:47 +0800

    解决冲突

--- view.py
+++ view.py
@@ -51,7 +51,7 @@ def befor_request():
 def show_blog(page=1):
     if page < 1:
         page = 1
-    p = date.get_entries_by_page(page=page, par_page=POST_PRE_PAGE)
+    p = date.get_articles_by_page(page=page, par_page=POST_PRE_PAGE)
     entries = p.items
     #页数标签
     if not p.total:
@@ -90,7 +90,7 @@ def show_tag(tag_id):
 
 @app.route('/entry/<int:entry_id>')
 def show_entry(entry_id):
-    entry = date.get_entry_by_id(entry_id)
+    entry = date.get_article_by_id(entry_id)
     date.increase_view_count(entry, 1)
     return render_template('/blog/show_entry.html', entry=entry,
                             **global_map())
@@ -220,3 +220,123 @@ def admin_index():
     return render_template('index.html')
 
 
+
+@app.route('/upload/', methods=['GET', 'POST', 'OPTIONS'])
+def upload():
+    """UEditor文件上传接口
+
+    config 配置文件
+    result 返回结果
+    """
+    mimetype = 'application/json'
+    result = {}
+    action = request.args.get('action')
+
+    # 解析JSON格式的配置文件
+    with open(os.path.join(app.static_folder, 'ueditor', 'php',
+                           'config.json')) as fp:
+        try:
+            # 删除 `/**/` 之间的注释
+            CONFIG = json.loads(re.sub(r'\/\*.*\*\/', '', fp.read()))
+        except:
+            CONFIG = {}
+
+    if action == 'config':
+        # 初始化时，返回配置文件给客户端
+        result = CONFIG
+
+    elif action in ('uploadimage', 'uploadfile', 'uploadvideo'):
+        # 图片、文件、视频上传
+        if action == 'uploadimage':
+            fieldName = CONFIG.get('imageFieldName')
+            config = {
+                "pathFormat": CONFIG['imagePathFormat'],
+                "maxSize": CONFIG['imageMaxSize'],
+                "allowFiles": CONFIG['imageAllowFiles']
+            }
+        elif action == 'uploadvideo':
+            fieldName = CONFIG.get('videoFieldName')
+            config = {
+                "pathFormat": CONFIG['videoPathFormat'],
+                "maxSize": CONFIG['videoMaxSize'],
+                "allowFiles": CONFIG['videoAllowFiles']
+            }
+        else:
+            fieldName = CONFIG.get('fileFieldName')
+            config = {
+                "pathFormat": CONFIG['filePathFormat'],
+                "maxSize": CONFIG['fileMaxSize'],
+                "allowFiles": CONFIG['fileAllowFiles']
+            }
+
+        if fieldName in request.files:
+            field = request.files[fieldName]
+            uploader = Uploader(field, config, app.static_folder)
+            result = uploader.getFileInfo()
+        else:
+            result['state'] = '上传接口出错'
+
+    elif action in ('uploadscrawl'):
+        # 涂鸦上传
+        fieldName = CONFIG.get('scrawlFieldName')
+        config = {
+            "pathFormat": CONFIG.get('scrawlPathFormat'),
+            "maxSize": CONFIG.get('scrawlMaxSize'),
+            "allowFiles": CONFIG.get('scrawlAllowFiles'),
+            "oriName": "scrawl.png"
+        }
+        if fieldName in request.form:
+            field = request.form[fieldName]
+            uploader = Uploader(field, config, app.static_folder, 'base64')
+            result = uploader.getFileInfo()
+        else:
+            result['state'] = '上传接口出错'
+
+    elif action in ('catchimage'):
+        config = {
+            "pathFormat": CONFIG['catcherPathFormat'],
+            "maxSize": CONFIG['catcherMaxSize'],
+            "allowFiles": CONFIG['catcherAllowFiles'],
+            "oriName": "remote.png"
+        }
+        fieldName = CONFIG['catcherFieldName']
+
+        if fieldName in request.form:
+            # 这里比较奇怪，远程抓图提交的表单名称不是这个
+            source = []
+        elif '%s[]' % fieldName in request.form:
+            # 而是这个
+            source = request.form.getlist('%s[]' % fieldName)
+
+        _list = []
+        for imgurl in source:
+            uploader = Uploader(imgurl, config, app.static_folder, 'remote')
+            info = uploader.getFileInfo()
+            _list.append({
+                'state': info['state'],
+                'url': info['url'],
+                'original': info['original'],
+                'source': imgurl,
+            })
+
+        result['state'] = 'SUCCESS' if len(_list) > 0 else 'ERROR'
+        result['list'] = _list
+
+    else:
+        result['state'] = '请求地址出错'
+
+    result = json.dumps(result)
+
+    if 'callback' in request.args:
+        callback = request.args.get('callback')
+        if re.match(r'^[\w_]+$', callback):
+            result = '%s(%s)' % (callback, result)
+            mimetype = 'application/javascript'
+        else:
+            result = json.dumps({'state': 'callback参数不合法'})
+
+    res = make_response(result)
+    res.mimetype = mimetype
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Headers'] = 'X-Requested-With,X_Requested_With'
+    return res
